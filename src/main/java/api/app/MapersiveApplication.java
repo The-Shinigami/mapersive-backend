e9e68e9d6c93dc48cbacd54910a33853e88c1577
The-Shinigami <mohamedbarbarr@gmail.com>
2023-08-23 20:27:38 +0100

    init project structure

---
+++ src/main/java/api/app/MapersiveApplication.java
@@ -0,0 +1,13 @@
+package api.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapersiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapersiveApplication.class, args);
+    }
+
+}
